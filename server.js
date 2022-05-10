6979b3b84c1ee45c1b08f17a68970f730312cfef
Ihor Klimov <ihor.klimov@ssa.group>
2022-05-10 09:23:55 -0400

    Add a handling for failed saved messages

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const schedule = require('node-schedule');
 const app = express()
 const port = process.env.PORT || 80
 fs = require('fs')
@@ -8,10 +9,10 @@ var jsonParser = bodyParser.json()
 
 var urlencodedParser = bodyParser.urlencoded({extended: false})
 
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const {initializeApp, applicationDefault, cert} = require('firebase-admin/app');
+const {getFirestore, Timestamp, FieldValue} = require('firebase-admin/firestore');
 const {credential} = require("firebase-admin");
-
+const failedMessages = [];
 const serviceAccount = require('./serviceAccountKey.json');
 app.use(express.urlencoded());
 app.use(express.json());
@@ -20,7 +21,23 @@ initializeApp({
     credential: cert(serviceAccount)
 });
 
+const rule = new schedule.RecurrenceRule();
+rule.hour = 0;
+rule.tz = 'Etc/UTC';
+
+const job = schedule.scheduleJob(rule, async function () {
+    await retrySendingFailedMessages();
+});
+
 const db = getFirestore();
+let shouldFailSaving = false;
+
+process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+    if (val === "--failSave") {
+        shouldFailSaving = true;
+    }
+});
 
 app.get('/v1/profile/:userId', async (req, res) => {
     const userId = req.params.userId;
@@ -40,9 +57,33 @@ app.post('/v1/sendMessage', async (req, res) => {
     const chatId = req.body.chatId;
     const message = req.body.message;
 
-    const snapshot = await db.collection('chats').doc(chatId).collection("messages").add({userId, message, timestamp: Timestamp.now()});
-    res.send({"result": "Success"});
+    let result = await sendMessage(chatId, userId, message);
+
+    res.send(result);
 })
+
+async function sendMessage(chatId, userId, message) {
+    let result;
+    try {
+        if (shouldFailSaving) {
+            throw new Error("Debug error");
+        }
+        const snapshot = await db.collection('chats').doc(chatId).collection("messages").add({
+            userId,
+            message,
+            timestamp: Timestamp.now()
+        });
+        result = {"result": "Success"};
+    } catch (e) {
+        result = {"result": "Database is offline. Will retry later"};
+        failedMessages.push({
+            userId,
+            chatId,
+            message,
+        })
+    }
+    return result;
+}
 
 app.get('/v1/getMessages/:chatId', async (req, res) => {
     const chatId = req.params.chatId;
@@ -61,6 +102,28 @@ app.get('/v1/getMessages/:chatId', async (req, res) => {
     res.send({"messages": result});
 })
 
+app.get('/v1/getFailedMessages', async (req, res) => {
+    res.send(failedMessages);
+})
+
+async function retrySendingFailedMessages() {
+    shouldFailSaving = false;
+
+    let i = failedMessages.length;
+    while (i--) {
+        let m = failedMessages[i];
+        let result = await sendMessage(m.chatId, m.userId, m.message);
+        if (result.result === "Success") {
+            failedMessages.splice(i, 1);
+        }
+    }
+}
+
+app.get('/v1/sendFailedMessages', async (req, res) => {
+    await retrySendingFailedMessages();
+    res.send("Done");
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
@@ -72,3 +135,4 @@ app.use(express.static(__dirname));
 // curl -X POST -H "Content-Type: application/json" -H "UserId: VGkotC32IMFVvdIpC3Yn" -d '{"name": "Ihor Klimov", "profileImage": "https://avatars.githubusercontent.com/u/13784275?v=4"}' localhost/v1/editProfile
 // curl -X POST -H "Content-Type: application/json" -H "UserId: VGkotC32IMFVvdIpC3Yn" -d '{"chatId": "EWEFGN@2n23mv", "message": "Hey there!"}' localhost/v1/sendMessage
 // curl -X GET -H "Content-Type: application/json" -H "UserId: VGkotC32IMFVvdIpC3Yn" localhost/v1/getMessages/EWEFGN@2n23mv
+// curl -X GET -H "Content-Type: application/json" -H "UserId: VGkotC32IMFVvdIpC3Yn" localhost/v1/getFailedMessages
